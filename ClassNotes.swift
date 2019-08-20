377290ecf9fa4e1e001dfa373a481839805c5673
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-20 11:10:32 -0400

    Adds class notes file

---
+++ ClassNotes.swift
@@ -0,0 +1,29 @@
+//
+//  ClassNotes.swift
+//  got-class
+//
+//  Created by David Rifkin on 8/20/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+/*
+ Why: to learn how to code
+ - to learn autolayout
+ - how to transfer data between VControllers
+ - how to use searchbar
+
+ What did you learn from this project
+🚙 vaguely, how to search through a matrix
+ - ex: Series[Season[Episode, Episode],
+                Season[Episode,Episode]]
+ - to prepare for multiple segues given prototype cells
+🚙 How many subclasses of UITableViewCell - 1
+ - checking for nil in segues, downcasting, searchbar
+ - how to read many errors!!
+ - how to use Property Observers (ex: willSet or didSet)
+
+ How: to do this
+ 
+ 
+ */
